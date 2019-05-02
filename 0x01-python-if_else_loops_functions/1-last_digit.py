7e702bb7531cfd021938be3092094346e0503d82
David Latorre <david@latorredev.com>
2019-05-02 12:12:34 -0500

    [Update] new version of script with negative numbers working

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,11 +3,15 @@ import random
 number = random.randint(-10000, 10000)
 
 print("Last digit of {0:d} is".format(number))
-
-number = number%10
-if number % 10 > 5:
+if number < 0:
+    number = number * -1
+    number = number % 10
+    number = number * -1
+else:
+    number = number % 10
+if number > 5:
     print(" {0:d} and is greater than 5".format(number))
-elif number % 10 == 0:
+elif number == 0:
     print(" 0 and is 0")
-elif number % 10 < 6:
+elif number < 6:
     print(" {0:d} and is less than 6 and not 0".format(number))
