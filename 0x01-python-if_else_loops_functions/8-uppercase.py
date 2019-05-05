0a2494a5d3f0f05f1fc9808b6c33c72c3af7e3de
David Latorre <david@latorredev.com>
2019-05-05 16:48:32 -0500

    [Feature] add format

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in range(len(str)):
-        print(chr(ord(str[i]) - 32)
-              if (ord(str[i]) > 96 and ord(str[i]) < 123)
-              else str[i], end='')
+        j = chr(ord(str[i]) - 32) \
+            if (ord(str[i]) > 96 and ord(str[i]) < 123) \
+            else str[i]
+        print('{}'.format(j), end="")
     print()
