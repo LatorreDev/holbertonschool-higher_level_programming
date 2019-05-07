355bf3214596a83130f73b465ed33463f4f56e95
David Latorre <david@latorredev.com>
2019-05-07 14:21:07 -0500

    [Update] refactoring code

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for i in range(len(my_list)):
-        aux = my_list[i]
-        print("{}".format(aux))
+        print("{0:d}".format(my_list[i]))
