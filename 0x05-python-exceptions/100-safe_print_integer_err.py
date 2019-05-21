bb4c202bc5367eff583715d62b097f674b42916e
David Latorre <david@latorredev.com>
2019-05-20 20:14:52 -0500

    [Fix] fix typo on the line 10

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,5 +7,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except Exception as inst:
-        print(inst, file=sys.stderr)
+        print("Exception: ", inst, file=sys.stderr)
         return False
