37ac2a465edaa9ac62bfd237e697be25885145ca
David Latorre <david@latorredev.com>
2019-05-23 20:42:19 -0500

    [Fix] fix cases

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    i = 1
-    print("{} arguments:".format(len(sys.argv)-1))
-    for i in range(len(sys.argv)):
-        if i == 0:
-            pass
-        else:
-            print("{:d}: {:}".format(i, (sys.argv[i])))
+    if len(sys.argv) == 1:
+        print("{:d} arguments:".format(len(sys.argv)-1))
+    elif len(sys.argv) == 2:
+        print("{:d} argument:".format(len(sys.argv)-1))
+        print("{:d}: {}".format(len(sys.argv) - 1, sys.argv[1]))
+    else:
+        print("{:d} arguments:".format(len(sys.argv)-1))
+        for i in range(1, len(sys.argv)):
+            print("{:d}: {}".format(i, (sys.argv[i])))
