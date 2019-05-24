0d1ade2dc7320f125dcdba3de89101d781658dbc
David Latorre <david@latorredev.com>
2019-05-23 20:07:24 -0500

    [Update] code improved

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    print("{}".format(len(sys.argv)-1))
+    i = 1
+    print("{} argument:".format(len(sys.argv)-1))
+    for i in range(len(sys.argv)):
+        if i == 0:
+            pass
+        else:
+            print("{}: {}".format(i, (sys.argv[i])))
