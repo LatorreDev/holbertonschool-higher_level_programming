be851c31993bc2c3844b8a528988c5a8a36cff02
David Latorre <david@latorredev.com>
2019-05-23 20:21:24 -0500

    [Fix] fix typo

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,9 +2,9 @@
 if __name__ == "__main__":
     import sys
     i = 1
-    print("{} argument:".format(len(sys.argv)-1))
+    print("{} arguments:".format(len(sys.argv)-1))
     for i in range(len(sys.argv)):
         if i == 0:
             pass
         else:
-            print("{}: {}".format(i, (sys.argv[i])))
+            print("{:d}: {:}".format(i, (sys.argv[i])))
