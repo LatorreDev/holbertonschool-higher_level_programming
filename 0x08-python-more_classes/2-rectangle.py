e5909f47bac3f8795530ad06a8d0f5a8538f2c68
David Latorre <david@latorredev.com>
2019-05-27 20:30:26 -0500

    [Update] Add option if height or width is 0

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -5,7 +5,10 @@ class Rectangle:
         self.__width = width
 
     def area(self):
-        return self.__height * self.__width
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        else:
+            return self.__height * self.__width
 
     def perimeter(self):
         return 2 * (self.__height + self.__width)
