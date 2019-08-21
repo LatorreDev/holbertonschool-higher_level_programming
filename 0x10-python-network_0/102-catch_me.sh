71c343f576ec8a4fabadd40d24c731558592dada
David Latorre <david@latorredev.com>
2019-08-20 23:43:24 -0500

    [UPDATE] modification on the order of the flags

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Delete Request
-curl -sXL PUT "" -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
+curl -sLX PUT "" -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
