6d1c1e712e32ae2746fb497312af5b71a6fff15b
David Latorre <david@latorredev.com>
2019-09-18 06:07:29 +0000

    [FIX] fix semistandard

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,19 +1,15 @@
 #!/usr/bin/node
 
-let number = parseInt(process.argv[2]);
+const number = parseInt(process.argv[2]);
 
-
-function factorial(number)
-{
-    if (number == 0){
-        return 1;
-    }
-    else if (process.argv.length == 2){
-	return 1;
-    }
-    else{
-        return number * factorial(number - 1);
-    }
+function factorial (number) {
+  if (number === 0) {
+    return 1;
+  } else if (process.argv.length === 2) {
+    return 1;
+  } else {
+    return number * factorial(number - 1);
+  }
 }
 
 console.log(factorial(number));
