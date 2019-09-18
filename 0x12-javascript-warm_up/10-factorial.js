38be59884989baeb25cad1628efe33b3c18f590d
David Latorre <david@latorredev.com>
2019-09-18 06:06:16 +0000

    [FEATURE] add javascript script to calculate afactorial recursively

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+let number = parseInt(process.argv[2]);
+
+
+function factorial(number)
+{
+    if (number == 0){
+        return 1;
+    }
+    else if (process.argv.length == 2){
+	return 1;
+    }
+    else{
+        return number * factorial(number - 1);
+    }
+}
+
+console.log(factorial(number));
